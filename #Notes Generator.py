debf45fe633e32ff8a62a0c2da5d0917d6eb7e6c
Isaac Cloud <isaaccloud@Isaacs-MacBook-Air.local>
2025-03-24 15:44:34 -0400

    Adding some variables

---
+++ #Notes Generator.py
@@ -0,0 +1,11 @@
+#Notes Generator
+
+device_type = input("What type of device?: ")
+asset_tag = input("What is the Asset Tag?: ")
+serial_number = input("What is the Serial Number?: ")
+kit_assigned = input("Was the device assigned to a kit?: Y/N "):
+    
+
+#if kit_assigned == Y:
+        #input("What is the Kit Name?")
+#summary = 
